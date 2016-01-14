4b12661a5d7bca20eb33199f1a478c216237e963
Samistine <Samistine@users.noreply.github.com>
2016-01-14 18:34:49 -0500

    Create Data.java

---
+++ Java-1/Lab-1/Data.java
@@ -0,0 +1,10 @@
+public class Data {
+    public static void main (String[] args) {
+        int age = 17;
+        int salary = 250000;
+        int iq = 129;
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("IQ: " + iq);
+    }
+}
