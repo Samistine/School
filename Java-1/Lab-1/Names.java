9ec6d06a0baf99707de0718626295caab6fce53d
Samistine <Samistine@users.noreply.github.com>
2016-01-14 18:11:33 -0500

    Create Names.java

---
+++ Java-1/Lab-1/Names.java
@@ -0,0 +1,8 @@
+public class Names {
+    public static void main (String[] args) {
+        String firstName = "Samuel";
+        String lastName = "Seidel";
+        System.out.println(firstName);
+        System.out.println(lastName);
+    }
+}
