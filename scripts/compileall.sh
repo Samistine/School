543b5ee29f3ee6ffa53c8716a27b4c54a543bdac
Samistine <samuel@samistine.com>
2016-01-28 13:59:06 -0500

    Update script to work on windows

--- scripts/compileall.sh
+++ scripts/compileall.sh
@@ -2,8 +2,8 @@ for f in *.java
 do
  echo "Compiling $f"
  javac $f
- echo "Executing ${f::-5}"
- java ${f::-5}
+ echo "Executing ${f:: ${#f}-5 }"
+ java ${f:: ${#f}-5 }
 done
 echo "Cleaning class files"
 rm *.class
