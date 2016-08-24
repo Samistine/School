9c83a3196814607a502a288293ceaf39f0912455
Samuel Seidel <samuel@samistine.com>
2016-08-23 20:15:09 -0400

    Begin Java-2

---
+++ Java-2/Unit00/src/main/java/com/samistine/school/java2/unit00/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.samistine.school.java2.unit00;
+
+/**
+ * Class: CIST 2372 Java II
+ * Quarter: Fall 2016
+ * Instructor: Dave Busse
+ * Project: Unit00
+ * Date: Created Aug 23, 2016 7:56:12 PM
+ * 
+ * By turning in this code, I Pledge:
+ *  1. That I have completed the programming assignment independently.
+ *  2. I have not copied the code from a student or any source.
+ *  3. I have not given my code to any student.
+ * 
+ * @author Samuel Seidel <samuel@samistine.com>
+ * @version 1.0
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
