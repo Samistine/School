d59cbc7f7c5098611923d6b6a1adab9723f407b5
Samistine <samuel@samistine.com>
2016-11-17 22:01:35 -0500

    Finished Java2-Unit06

--- Java-2/Unit06/src/main/java/com/samistine/school/java2/unit06/Faculty.java
+++ Java-2/Unit06/src/main/java/com/samistine/school/java2/unit06/Faculty.java
@@ -1,7 +1,6 @@
 package com.samistine.school.java2.unit06;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -71,10 +70,7 @@ public class Faculty {
     private String rank;
     private double salary;
     private String deptId;
-    
-    //noargs - useless DO NOT USE
-    public Faculty() {}
-    
+
     //Inserts if doesn't exists, else updates - undortunate side effect
     public Faculty(String ssn, String firstName, char mi, String lastName,
     String phone, String email, String office, String startTime,
@@ -90,14 +86,35 @@ public class Faculty {
         this.rank = rank;
         this.salary = salary;
         this.deptId = deptId;
-        Faculty.toDB(this);
+        //Save to database, overrides existing entry on duplicate key(SSN)
+        saveToDB();
     }
 
     public Faculty(String ssn) throws SQLException, IllegalArgumentException {
-        Faculty facultyDB;
+        this.ssn = ssn;
+        loadFromDB();
+    }
+    
+    //delete the object in the underlying database
+    public void deleteDB() throws SQLException {
+        deleteDB(ssn);
+    }
+    
+    //Update database with current values
+    public void updateDB() throws SQLException {
+        saveToDB();
+    }
+
+    /**
+     * Discards any data and gets the state directly from the database using the
+     * faculty's ssn.
+     * 
+     * @throws Exception 
+     */
+    void loadFromDB() throws SQLException {
         try (
         //Get the connection to the database
-        final Connection conn = DB.connect();
+        final Connection conn = DB.getInstance().connect();
         //Create a prepared statement
         final PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM Faculty WHERE ssn = ?")) {
             //Set the last and only argument to the ssn from the method call
@@ -108,113 +125,64 @@ public class Faculty {
             final ResultSet rs = pstmt.executeQuery()) {
                 //Check whether we got any results back, we're expecting one or none.
                 if (rs.next()) {
-                    //We got one, lets convert it to a POJO and return it.
-                    facultyDB = fromDB(rs);
+                    this.ssn = rs.getString("ssn");
+                    this.firstName = rs.getString("firstName");
+                    this.mi = rs.getString("mi").charAt(0);
+                    this.lastName = rs.getString("lastName");
+                    this.phone = rs.getString("phone");
+                    this.email = rs.getString("email");
+                    this.office = rs.getString("office");
+                    this.startTime = rs.getString("startTime");
+                    this.rank = rs.getString("rank");
+                    this.salary = rs.getDouble("salary");
+                    this.deptId = rs.getString("deptId");
                 } else {
                     //We got nothing, return null
                     throw new IllegalArgumentException("There is not an entry for ssn: " + ssn);
                 }
             }
         }
-        this.ssn = facultyDB.getSSN();
-        this.firstName = facultyDB.getFirstName();
-        this.mi = facultyDB.getMiddleInitial();
-        this.lastName = facultyDB.getLastName();
-        this.phone = facultyDB.getPhone();
-        this.email = facultyDB.getEmail();
-        this.office = facultyDB.getOffice();
-        this.startTime = facultyDB.getStartTime();
-        this.rank = facultyDB.getRank();
-        this.salary = facultyDB.getSalary();
-        this.deptId = facultyDB.getDepartmentID();
-    }
-    
-    //delete the object in the underlying database
-    public void deleteDB() throws SQLException {
-        deleteDB(ssn);
-    }
-    
-    //Update database with current values
-    public void updateDB() throws SQLException {
-        toDB(this);
-    }
-
-    //Print out, ide does not have auto toString
-    void print() {
-        System.out.println("Printing Faculty Fields");
-        for (java.lang.reflect.Field field : getClass().getDeclaredFields()) {
-            try {
-                //field.setAccessible(true);
-                String name = field.getName();
-                Object value = field.get(this);
-                System.out.printf("%s, Field value: %s%n", name, value);
-            } catch (IllegalAccessException ex) {
-                ex.printStackTrace();
-            }
-        }
-        System.out.println("Done \r\n");
-    }
-
-
-    /*
-     * Convert from resultset
-     */
-    static Faculty fromDB(ResultSet rs) throws SQLException {
-        //Translate Fields below
-        final String ssn = rs.getString("ssn");
-        final String firstName = rs.getString("firstName");
-        final char mi = rs.getString("mi").charAt(0);
-        final String lastName = rs.getString("lastName");
-        final String phone = rs.getString("phone");
-        final String email = rs.getString("email");
-        final String office = rs.getString("office");
-        final String startTime = rs.getString("startTime");
-        final String rank = rs.getString("rank");
-        final double salary = rs.getDouble("salary");
-        final String deptId = rs.getString("deptId");
-
-        return new Faculty(ssn, firstName, mi, lastName, phone, email, office, startTime, rank, salary, deptId);
     }
     
     /**
      * Inserts or updates the database with and for the following faculty object
      */
-    static void toDB(Faculty pojo) throws SQLException {
+    void saveToDB() throws SQLException {
         //SQL Code
-        String sql = "INSERT INTO Faculty" +
-        "  (ssn, firstName, mi, lastName, phone, email, office, startTime, rank, salary, deptId)"+
-        "VALUES" +
-        "  (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" +
-        "ON DUPLICATE KEY UPDATE"+
-        "  ssn     = VALUES(ssn),"+
+        final String sql = "" +
+        "INSERT INTO Faculty (ssn, firstName, mi, lastName, phone, email, office, startTime, rank, salary, deptId) "+
+        "VALUES  (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) "+
+        "ON DUPLICATE KEY UPDATE "+
+        "  ssn           = VALUES(ssn),"+
         "  firstName     = VALUES(firstName),"+
-        "  mi     = VALUES(mi),"+
-        "  lastName     = VALUES(lastName),"+
-        "  phone     = VALUES(phone),"+
-        "  email     = VALUES(email),"+
-        "  office     = VALUES(office),"+
+        "  mi            = VALUES(mi),"+
+        "  lastName      = VALUES(lastName),"+
+        "  phone         = VALUES(phone),"+
+        "  email         = VALUES(email),"+
+        "  office        = VALUES(office),"+
         "  startTime     = VALUES(startTime),"+
-        "  rank     = VALUES(office),"+
-        "  salary     = VALUES(salary),"+
-        "  deptId = VALUES(deptId)";
+        "  rank          = VALUES(office),"+
+        "  salary        = VALUES(salary),"+
+        "  deptId        = VALUES(deptId)";
         
         try (
         //Get the connection to the database
-        final Connection conn = DB.connect();//connect
+         final Connection conn = DB.getInstance().connect();//connect
         //Create a prepared statement
-        final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+         final PreparedStatement pstmt = conn.prepareStatement(sql)) {
             //Set pstmt args below
-            pstmt.setString(1, pojo.getSSN());
-            pstmt.setString(2, pojo.getFirstName());
-            pstmt.setString(3, pojo.getMiddleInitial() + "");
-            pstmt.setString(4, pojo.getLastName());
-            pstmt.setString(5, pojo.getPhone());
-            pstmt.setString(6, pojo.getEmail());
-            pstmt.setString(7, pojo.getOffice());
-            pstmt.setString(8, pojo.getStartTime());
-            pstmt.setString(9, pojo.getRank());
-            pstmt.setDouble(10, pojo.getSalary());
-            pstmt.setString(11, pojo.getDepartmentID());
+            pstmt.setString(1, this.ssn);
+            pstmt.setString(2, this.firstName);
+            pstmt.setString(3, String.valueOf(this.mi));//char
+            pstmt.setString(4, this.lastName);
+            pstmt.setString(5, this.phone);
+            pstmt.setString(6, this.email);
+            pstmt.setString(7, this.office);
+            pstmt.setString(8, this.startTime);
+            pstmt.setString(9, this.rank);
+            pstmt.setDouble(10, this.salary);
+            pstmt.setString(11, this.deptId);
+            //Execute
             pstmt.execute();//execute
         }
     }
@@ -224,54 +192,10 @@ public class Faculty {
      */
     public static void deleteDB(String ssn) throws SQLException {
         try (
-        Connection conn = DB.connect();//connect
-        PreparedStatement stmt = conn.prepareCall("DELETE FROM Faculty where ssn = ?");) {
+         final Connection conn = DB.getInstance().connect();//connect
+         final PreparedStatement stmt = conn.prepareCall("DELETE FROM Faculty where ssn = ?");) {
             stmt.setString(1, ssn);
             stmt.execute();//execute
         }
     }
-}
-
-//Inlined for delivery to blackboard
-class DB {
-    
-//    private static DB INSTANCE;
-//    
-//    public synchronized static DB getInstance() {
-//        if (INSTANCE == null) INSTANCE = new DB();
-//        return INSTANCE;
-//    }
-    
-    static String url = "jdbc:mysql://node3.codenvy.io:32904/javabook?autoReconnect=true&useSSL=false";
-     //String url = "jdbc:mysql://localhost/javabook?autoReconnect=true&useSSL=false";
-    static String username = "scott";
-    static String password = "tiger";
-    
-    private static Connection db;
-    
-    
-    public static Connection connect() throws SQLException {
-        if (!isConnected()) {
-            if (db != null) {
-                System.out.println("unexpectedly disconnected from database");
-                db = null;
-            }
-            db = DriverManager.getConnection(url, username, password);
-            db.setAutoCommit(true);
-            //db.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ);
-            System.out.println("connected to database");
-        }
-        return db;
-    }
-
-    //See if we need to reconnect
-    public static boolean isConnected() {
-        try {
-            return (db != null) && (!db.isClosed()) && db.isValid(1);
-        } catch (Exception se) { //Changed from SQLException to all exceptions
-            se.printStackTrace();
-            return false;
-        }
-    }
-
 }
--- Java-2/Unit06/src/main/java/com/samistine/school/java2/unit06/FacultyTester2.java
+++ Java-2/Unit06/src/main/java/com/samistine/school/java2/unit06/FacultyTester2.java
@@ -1,6 +1,6 @@
 package com.samistine.school.java2.unit06;
 
-public class FacultyTester {
+public class FacultyTester2 {
     public static void main(String[] args) throws Exception {
         //Preload
         try {
@@ -9,26 +9,27 @@ public class FacultyTester {
             Class.forName("com.samistine.school.java2.unit06.DB");
         } catch (Exception ex) {
             System.err.println("Couldn't preload");
+            ex.printStackTrace();
         }
         
         long begin = System.currentTimeMillis();
         //First Test
-        print(new Faculty("111221110"));
+        print(new Faculty2("111221110"));
 
         //Second Test
-        Faculty sam = new Faculty(
+        Faculty2 sam = new Faculty2(
         "9999", "Samuel", 'R', "Seidel", "8000000001", "samuel@samistine.com", 
         "Computers", "2011-11-11", "Professor", 100_000_000, "22");
         
         sam.setFirstName("Devon");
         sam.updateDB();
         
-        print(new Faculty("9999"));
+        print(new Faculty2("9999"));
         
         sam.deleteDB();
         
         try {
-            print(new Faculty("9999"));
+            print(new Faculty2("9999"));
         } catch (IllegalArgumentException ex) {
             System.out.println("Works");
         }
@@ -40,7 +41,7 @@ public class FacultyTester {
     
     //Print object that doesn't implement toString
     public static void print(Object o) {
-        System.out.println("Printing Faculty Fields");
+        System.out.println("Printing Faculty2 Fields");
         for (java.lang.reflect.Field field : o.getClass().getDeclaredFields()) {
             try {
                 field.setAccessible(true);
